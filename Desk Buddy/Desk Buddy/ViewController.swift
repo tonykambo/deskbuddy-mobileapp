58f335f398e27cb40e3ab94bb21ed8ecd53ab8e1
Tony Kambourakis <akambour@au1.ibm.com>
2017-04-13 22:00:48 +1000

    Initial project template

---
+++ Desk Buddy/Desk Buddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Desk Buddy
+//
+//  Created by Tony Kambourakis on 13/4/17.
+//  Copyright © 2017 Kamboville. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
