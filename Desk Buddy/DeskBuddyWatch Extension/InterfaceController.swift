dffa3888055dd017fec5ea78e3c400d861fcd9d6
Tony Kambourakis <akambour@au1.ibm.com>
2017-04-22 07:15:53 +1000

    Added Watch app target

---
+++ Desk Buddy/DeskBuddyWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  DeskBuddyWatch Extension
+//
+//  Created by Tony Kambourakis on 20/4/17.
+//  Copyright © 2017 Kamboville. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
