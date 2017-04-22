5874532ebe0fe81448708e6ed011245b4de9fe29
Tony Kambourakis <akambour@au1.ibm.com>
2017-04-23 08:43:49 +1000

    Added app icon and cleaned up graph view

--- Desk Buddy/Desk Buddy/ViewController.swift
+++ Desk Buddy/Desk Buddy/ViewController.swift
@@ -10,89 +10,49 @@ import UIKit
 import SwiftChart
 import Charts
 
-
 class ViewController: UIViewController {
     
     @IBOutlet weak var chart: LineChartView!
-    
     @IBOutlet weak var awayButton: UIButton!
-    
     @IBOutlet weak var atWorkButton: UIButton!
-    
     var climateReadings: [Climate]!
 
-//    override var preferredStatusBarStyle: UIStatusBarStyle {
-//        return UIStatusBarStyle.lightContent
-//    }
-
-    
     override func viewWillAppear(_ animated: Bool) {
         UIApplication.shared.statusBarStyle = .lightContent
     }
     
     @IBAction func selectAway(_ sender: UIButton) {
-        
         self.atWorkButton.layer.borderWidth = 0
-        
         self.awayButton.layer.cornerRadius = 8.0
-        
         self.awayButton.layer.borderColor = UIColor.white.cgColor
-        
         self.awayButton.layer.borderWidth = 2.0
         self.awayButton.clipsToBounds = true
-        
     }
     
     
     @IBAction func selectAtWork(_ sender: UIButton) {
-        
-        
         self.awayButton.layer.borderWidth = 0
-        
-        
-        
         self.atWorkButton.layer.cornerRadius = 8.0
-        
         self.atWorkButton.layer.borderColor = UIColor.white.cgColor
-        
         self.atWorkButton.layer.borderWidth = 2.0
         self.atWorkButton.clipsToBounds = true
-        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
-        
-        //chart.delegate = self
-        
-        
         let blueColor = UIColor(colorLiteralRed: 0x02/255.0, green: 0x68/255.0, blue: 0xA2/255.0, alpha: 0xFF/255.0)
-        
-        
         self.awayButton.layer.cornerRadius = 8.0
-        
         self.awayButton.layer.borderColor = UIColor.white.cgColor
-        
         self.awayButton.layer.borderWidth = 2.0
         self.awayButton.clipsToBounds = true
-        
-        
-        
-//        UINavigationBar.appearance().backgroundColor = blueColor
-//        UINavigationBar.appearance().tintColor = blueColor
-        
-        
         self.navigationController?.navigationBar.barTintColor = blueColor
         self.navigationController?.navigationBar.tintColor = UIColor.white
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
-        
-
-        chart.noDataText = "No climate data available."
-        
+        chart.noDataText = "Waiting for climate data."
         updateWeather()
     }
     
@@ -101,17 +61,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func didTouchChart(_ chart: Chart, indexes: Array<Int?>, x: Float, left: CGFloat) {
-        for (serieIndex, dataIndex) in indexes.enumerated() {
-            if dataIndex != nil {
-                // The series at serieIndex has been touched
-                let value = chart.valueForSeries(serieIndex, atIndex: dataIndex)
-            }
-        }
-    }
-    
-    
-    
     
     
     func updateClimateGraph(climateReadings: [Climate]) {
@@ -119,132 +68,97 @@ class ViewController: UIViewController {
         let timeFormatter = DateFormatter()
         timeFormatter.dateFormat = "HH:mm"
         
+        // Prepare temperature data Set
         var temperatureDataEntries: [ChartDataEntry] = []
         for i in 0..<climateReadings.count {
             let dataEntry = ChartDataEntry(x: Double(i), y: climateReadings[(climateReadings.count-1)-i].temperature)
             temperatureDataEntries.append(dataEntry)
         }
         let temperatureDataSet = LineChartDataSet(values: temperatureDataEntries, label: "Temperature (°C)")
-        
-        
         let redColor = UIColor(colorLiteralRed: 0xFC/255.0, green: 0x3E/255.0, blue: 0x30/255.0, alpha: 0xFF/255.0)
-
-      
-        
-        
         temperatureDataSet.circleColors = [redColor]
         temperatureDataSet.setColor(redColor)
         temperatureDataSet.circleRadius = 2.0
         temperatureDataSet.valueFont = UIFont(name: "Helvetica", size: 14)!
         temperatureDataSet.valueColors = [redColor]
-    
         
+        // Prepare humidity data set
         var humidityDataEntries: [ChartDataEntry] = []
         for i in 0..<climateReadings.count {
             let dataEntry = ChartDataEntry(x: Double(i), y: climateReadings[(climateReadings.count-1)-i].humidity)
             humidityDataEntries.append(dataEntry)
         }
         let humidityDataSet = LineChartDataSet(values: humidityDataEntries, label: "Humidity (%)")
-        
-       
         let blueColor = UIColor(colorLiteralRed: 0x0F/255.0, green: 0x7D/255.0, blue: 0xB5/255.0, alpha: 0xFF/255.0)
-        
-    
         humidityDataSet.circleColors = [blueColor]
         humidityDataSet.circleColors = [blueColor]
         humidityDataSet.setColor(blueColor)
         humidityDataSet.circleRadius = 2.0
         humidityDataSet.valueFont = UIFont(name: "Helvetica", size: 14)!
         humidityDataSet.valueColors = [blueColor]
-    
+        
+        // Right (Y) Axis Configuration
+        
+        chart.rightAxis.axisMinimum = 0
+        chart.rightAxis.enabled = false
+        chart.rightAxis.drawLabelsEnabled = false
+        chart.rightAxis.drawGridLinesEnabled = false
+        chart.doubleTapToZoomEnabled = false
+        //chart.dragEnabled = false
+        chart.rightAxis.drawAxisLineEnabled = false
+        
+        
+        // Left (Y) Axis Configuration
+        
+        chart.leftAxis.drawGridLinesEnabled = true
+        chart.leftAxis.gridLineWidth = 0.1
+        chart.leftAxis.gridColor = UIColor.lightGray
+//        chart.leftAxis.drawLabelsEnabled = false
+        // the line next to the left axis labels
+        chart.leftAxis.drawAxisLineEnabled = false
+        chart.leftAxis.labelTextColor = UIColor.lightGray
+        chart.leftAxis.labelFont = UIFont(name: "Helvetica", size: 14)!
+//        chart.xAxis.avoidFirstLastClippingEnabled = true
+//        chart.leftAxis.axisMinimum = 0
+
+        // X Axis Configuration
+        
+        chart.xAxis.drawGridLinesEnabled = false
+        // the line next to the x axis labels
+        chart.xAxis.drawAxisLineEnabled = false
+        chart.xAxis.granularityEnabled = true
+        chart.xAxis.granularity = 1.0
+        chart.xAxis.labelPosition = XAxis.LabelPosition.bottom
+        chart.xAxis.labelTextColor = UIColor.lightGray
+        chart.xAxis.labelFont = UIFont(name: "Helvetica", size: 14)!
+        chart.xAxis.spaceMin = 0.3
+        chart.xAxis.spaceMax = 0.3
         
         // Set the x Axis labels
         
         let times = climateReadings.map { (climate) -> String in
             timeFormatter.string(from: climate.dateMeasured)
         }
-        
         let chartFormatter = ChartFormatter(labels: times.reversed())
         let xAxis = XAxis()
         xAxis.valueFormatter = chartFormatter
         chart.xAxis.valueFormatter = xAxis.valueFormatter
-        
         xAxis.granularityEnabled = true
         
-       
-        // Change the minimum value of the left axis
         
-        //chart.leftAxis.axisMinimum = 0
-        
-        
-        // Right Axis Configuration
-        
-        chart.rightAxis.axisMinimum = 0
-        chart.rightAxis.enabled = false
-        chart.rightAxis.drawLabelsEnabled = false
-       
-        chart.doubleTapToZoomEnabled = false
-        //chart.dragEnabled = false
-        
-        
-        chart.leftAxis.drawGridLinesEnabled = true
-        chart.rightAxis.drawGridLinesEnabled = false
-        
-        // Set the x axis grid lines to be light gray and wafer thin
-        
-        chart.leftAxis.gridLineWidth = 0.1
-        chart.leftAxis.gridColor = UIColor.lightGray
-        
-        chart.xAxis.drawGridLinesEnabled = false
-        
-    
-        //chart.leftAxis.drawLabelsEnabled = false
-        
-        
-        chart.rightAxis.drawAxisLineEnabled = false
-        //chart.grid
-        
+        // General Chart Configuration
         
         chart.chartDescription?.text = ""
         chart.drawGridBackgroundEnabled = false
         chart.drawBordersEnabled = false
-        
-        // the line next to the left axis labels
-        
-        chart.leftAxis.drawAxisLineEnabled = false
-        
-        // the line next to the x axis labels
-        
-        chart.xAxis.drawAxisLineEnabled = false
-        
-        var climateDataSet: [LineChartDataSet] = []
-        
-        climateDataSet.append(temperatureDataSet)
-        climateDataSet.append(humidityDataSet)
-        
-        let lineChartData = LineChartData(dataSets: climateDataSet)
-
-        chart.xAxis.granularityEnabled = true
-        chart.xAxis.granularity = 1.0
-        
-        chart.xAxis.labelPosition = XAxis.LabelPosition.bottom
-
-        chart.xAxis.labelTextColor = UIColor.lightGray
-        chart.leftAxis.labelTextColor = UIColor.lightGray
-        
-        chart.xAxis.labelFont = UIFont(name: "Helvetica", size: 14)!
-        chart.leftAxis.labelFont = UIFont(name: "Helvetica", size: 14)!
-        
-        
+        chart.legend.font = UIFont(name: "Helvetica", size: 14)!
         chart.backgroundColor = UIColor.white
         
-        chart.xAxis.spaceMin = 0.3
-        chart.xAxis.spaceMax = 0.3
         
-        //chart.xAxis.avoidFirstLastClippingEnabled = true
-        
-        chart.legend.font = UIFont(name: "Helvetica", size: 14)!
-        
+        var climateDataSet: [LineChartDataSet] = []
+        climateDataSet.append(temperatureDataSet)
+        climateDataSet.append(humidityDataSet)
+        let lineChartData = LineChartData(dataSets: climateDataSet)
         chart.data = lineChartData
 
     }
